979f8e9ff2434830f6f0fbeabaed9d0efe389089
OmarGuz5 <omarguz225@gmail.com>
2018-08-17 11:07:48 -0400

    Added answers

---
+++ ClientSideDemo/closure.js
@@ -0,0 +1,16 @@
+//behaviro that will execute when page loads
+window.onload = function() {
+    console.log("page loaded!");
+    counter = getCount();
+}
+function getCount(noun) {
+    var count = 0;
+    return function() {
+        count += 1;
+        if(noun) {
+            console.log(noun + "count is "+ count);
+        } else {
+            console.log("count is: "+count);
+        }
+    }
+}
